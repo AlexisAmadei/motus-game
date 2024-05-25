6a896adf081b15077d885f77bc2b47927e86c1d9
Alexis Amadei <alexisamadei@gmail.com>
2024-05-25 18:38:41 +0200

    Update App style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import secretWords from './utils/secretWords'
 import GameGrid from './components/GameGrid'
+import './styles/App.css'
 
 export default function App() {
   const gameId = "id" + Math.random().toString(16).slice(2);
@@ -19,13 +20,6 @@ export default function App() {
     }));
   }, []);
 
-  // async function fetchWords() {
-  //   setGameData({
-  //     ...gameData,
-  //     words: await secretWords().then(console.debug('words fetched')),
-  //   });
-  // }
-
   useEffect(() => {
     fetchWords();
   }, [fetchWords]);
@@ -39,24 +33,10 @@ export default function App() {
     localStorage.setItem('gameData', JSON.stringify(data));
   }, [gameId, gameData]);
 
-  // function storeGameData() {
-  //   const data = {
-  //     gameId: gameId,
-  //     level: gameData.level,
-  //     words: gameData.words,
-  //   }
-  //   localStorage.setItem('gameData', JSON.stringify(data));
-  // }
-
   const handleGameStart = useCallback(() => {
     storeGameData();
     setGameStarted(true);
   }, [storeGameData]);
-
-  // function handleGameStart() {
-  //   storeGameData();
-  //   setGameStarted(true);
-  // }
 
   return (
     <div className='app-wrapper'>
